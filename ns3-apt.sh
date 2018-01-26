97d3905f29cc689d06fed95a589dacb27df68733
Fang He <hf.enigma@gmail.com>
2018-01-27 00:44:42 +0800

    fix python pip & directories

--- ns3-apt.sh
+++ ns3-apt.sh
@@ -10,12 +10,13 @@ sudo apt-get upgrade
 #sudo apt-get install gcc-4.9 g++-4.9
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
-sudo apt-get install gcc g++ python python-dev mercurial python-setuptools git qt5-default python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython openmpi-bin openmpi-common openmpi-doc libopenmpi-dev autoconf cvs bzr unrar gdb valgrind uncrustify gsl-bin flex bison libfl-dev tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev cmake libc6-dev libc6-dev-i386 libclang-dev libgtk2.0-0 libgtk2.0-dev  libboost-signals-dev libboost-filesystem-dev
+sudo apt-get install gcc g++ python python-dev python-pip mercurial python-setuptools git qt5-default python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython openmpi-bin openmpi-common openmpi-doc libopenmpi-dev autoconf cvs bzr unrar gdb valgrind uncrustify gsl-bin flex bison libfl-dev tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev cmake libc6-dev libc6-dev-i386 libclang-dev libgtk2.0-0 libgtk2.0-dev  libboost-signals-dev libboost-filesystem-dev
 sudo pip install cxxfilt
 cd
-mkdir projects
-cd projects
+mkdir -p ~/projects
+cd ~/projects
 hg clone http://code.nsnam.org/ns-3-allinone
+cd ns-3-allinone
 ./download.py -n ns-3.27
 ./build.py
 ./test.py
