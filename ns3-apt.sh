f6125209cea6f142b2ae7f3551842a12a74e39f1
Fang He <hf.enigma@gmail.com>
2018-01-27 12:58:49 +0800

    fix openflow in debian series

--- ns3-apt.sh
+++ ns3-apt.sh
@@ -15,14 +15,24 @@ sudo apt-get install build-essential
 #sudo apt-get install gcc-4.9 g++-4.9
 
 sudo apt-get install python python-dev python-pip python-setuptools mercurial git qt5-default openmpi-bin openmpi-common openmpi-doc libopenmpi-dev autoconf cvs bzr unrar gdb valgrind uncrustify gsl-bin flex bison libfl-dev tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev cmake libc6-dev libc6-dev-i386 libclang-dev libgtk2.0-0 libgtk2.0-dev libboost-signals-dev libboost-filesystem-dev
+
+# python API is experimental
 #sudo apt-get install python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython
 
 sudo pip install cxxfilt
 
 mkdir -p ~/projects
 cd ~/projects
-hg clone http://code.nsnam.org/ns-3-allinone
+
+# build openflow
 hg clone http://code.nsnam.org/openflow
-cd ns-3-allinone
+cd openflow
+./waf configure
+./waf build
+
+# build ns3
+hg clone http://code.nsnam.org/ns-3-allinone
+cd ../ns-3-allinone
 ./download.py -n ns-3.27
-./build.py --build-options=--progress -- --build-profile=debug --prefix=/usr --disable-python --with-openflow=../openflow
+# use $HOME or it will fail
+./build.py --build-options=--progress -- --build-profile=debug --prefix=/usr --disable-python --with-openflow=$HOME/projects/openflow
